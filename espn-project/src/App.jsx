18cd813ccca8f8d49f34da9ca22c6a86f9ba5a47
keanu934 <keanucrocker@gmail.com>
2026-05-15 12:45:33 -0400

    nein

--- espn-project/src/App.jsx
+++ espn-project/src/App.jsx
@@ -1,17 +1,19 @@
-import {Routes, Route, Link} from "react-router-dom"
-import BaseBall from "./pages/BaseBall"
-import Football from "./pages/Football"
-import Soccer from "./pages/Soccer"
+import { Routes, Route, Link } from "react-router-dom";
+import BaseBall from "./pages/BaseBall";
+import Football from "./pages/Football";
+import Soccer from "./pages/Soccer";
+import Nav from "./components/nav";
+import Home from "./pages/Home";
 
 function App() {
-    return (
-        <>
-        <Nav></Nav>
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/baseball"></Route>
-        </Routes>
-        </>
-    )
+  return (
+    <>
+      <Nav></Nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/baseball" element={<BaseBall />}></Route>
+      </Routes>
+    </>
+  );
 }
 export default App;
