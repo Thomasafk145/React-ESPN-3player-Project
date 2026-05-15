7c5ae2baf2c47232683173016e99e4b7a4bc9fdb
keanu934 <keanucrocker@gmail.com>
2026-05-15 12:51:04 -0400

    night

--- espn-project/src/components/nav.jsx
+++ espn-project/src/components/nav.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 function Nav() {
-  return 
-  <>
-   <nav>
-    <Link to="/baseball">Baseball</Link>
-    <Link to="/football">Football</Link>
-    <Link to="/soccer">Soccer</Link>
-   </nav>
-  </>;
+  return (
+    <>
+      <nav>
+        <Link to="/baseball">Baseball | </Link>
+        <Link to="/football">Football | </Link>
+        <Link to="/soccer">Soccer | </Link>
+      </nav>
+    </>
+  );
 }
 export default Nav;
--- espn-project/src/pages/Home.jsx
+++ espn-project/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Nav from "../components/nav";
 function Home() {
   return (
     <>
       <h1>Home Page</h1>
 
       <p>Welcome to the website!</p>
+      <Nav></Nav>
     </>
   );
 }
