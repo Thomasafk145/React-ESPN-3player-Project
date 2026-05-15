298d19a9f27ca82100418a5144335944d47fea88
Your Name <youremail@yourdomain.com>
2026-05-15 12:38:28 -0400

    hi

--- espn-project/src/App.jsx
+++ espn-project/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import {Routes, Route, Link} from "react-router-dom"
+import BaseBall from "./pages/BaseBall"
+import Football from "./pages/Football"
+import Soccer from "./pages/Soccer"
 
+function App() {
+    return (
+        <>
+        <Nav></Nav>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+        </Routes>
+        </>
+    )
+}
 export default App;
--- espn-project/src/components/nav.jsx
+++ espn-project/src/components/nav.jsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
 function Nav() {
-  return <></>;
+  return 
+  <>
+   <nav>
+    <Link to="/baseball">Baseball</Link>
+    <Link to="/football">Football</Link>
+    <Link to="/soccer">Soccer</Link>
+   </nav>
+  </>;
 }
 export default Nav;
