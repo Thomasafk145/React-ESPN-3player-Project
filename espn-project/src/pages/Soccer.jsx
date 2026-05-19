c42356e1c5be76272f86a2c3999f4e0dcf3c319b
Thomas <thomascarp3nt3r@gmail.com>
2026-05-19 12:18:49 -0400

    urls

--- espn-project/src/pages/Soccer.jsx
+++ espn-project/src/pages/Soccer.jsx
@@ -1,4 +1,7 @@
 export default function Soccer() {
+  let SoccerNews_url = "https://www.espn.com/soccer/";
+  let SoccerTeams_url =
+    "https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/teams";
   return (
     <>
       <h1>This is our page about Soccer</h1>
